d54ef4be9c3d775bfd11a3f9eba3eb5f25e28f8d
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-03-29 19:53:15 -0700

    Instagram button now leads to a page with instagram color gradient background + specified width and height of the container

--- popup.js
+++ popup.js
@@ -1,5 +1,8 @@
 const instagram = document.getElementById("instagram");
+const container = document.querySelector(".container");
+const instagramContainer = document.querySelector(".instagramContainer");
 
 instagram.onclick = () => {
-    
-}
+    container.classList.add("container-hidden");
+    instagramContainer.style.display = "flex";
+}   
