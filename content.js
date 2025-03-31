b1e04729881ed41f8dd4084cd726521a9476bd39
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-03-30 21:30:32 -0700

    The user now can decide whether he wants to see posts or not

--- content.js
+++ content.js
@@ -1,13 +1,29 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === "removePosts") {
-        removePosts();
-        sendResponse({status: "success"});
-    }
-})
+const instagramPosts = document.querySelector(".xw7yly9").firstChild.nextSibling;
+const instagramPostsParent = document.querySelector(".xw7yly9");
 
 function removePosts() {
-    const instagramPosts = document.querySelector(".xw7yly9").firstChild.nextSibling;
     instagramPosts.remove();
 }
+
+function returnPosts() {
+    instagramPostsParent.appendChild(instagramPosts);
+}
+
+chrome.storage.local.get("hidePosts", (data) => {
+    const shouldHide = data.hidePosts;
+    if (shouldHide) {
+        removePosts();
+    }
+});
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === "removePosts") {
+        removePosts();
+        sendResponse({ result: "Posts removed" });
+    } else if (message.action === "returnPosts") {
+        returnPosts();
+        sendResponse({ result: "Posts returned" });
+    }
+});
 
 
--- popup.js
+++ popup.js
@@ -4,13 +4,37 @@ const instagramContainer = document.querySelector(".instagramContainer");
 const instaSwitch = document.getElementById("instaSwitch__inner");
 const checkBox = instaSwitch.querySelector("input");
 
+document.addEventListener("DOMContentLoaded", () => {
+    chrome.storage.local.get("hidePosts", (data) => {
+        let isChecked = data.hidePosts;
+        checkBox.checked = isChecked;
+
+        const action = isChecked ? "removePosts" : "returnPosts";
+
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            chrome.tabs.sendMessage(tabs[0].id, { action }, (response) => {
+                console.log("Response from content.js:", response?.result);
+            });
+        });
+    })
+})
+
 instagram.onclick = () => {
     container.classList.add("container-hidden");
     instagramContainer.style.display = "flex";
 }   
 
 checkBox.addEventListener("change", () => {
-    if (checkBox.checked) {
-        
-    }
+    let isChecked = checkBox.checked
+
+    // Save to storage
+    chrome.storage.local.set({hidePosts: isChecked});
+
+    const action = isChecked ? "removePosts" : "returnPosts";
+
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, { action }, (response) => {
+            console.log("Response from content.js:", response?.result);
+        });
+    });
 });
